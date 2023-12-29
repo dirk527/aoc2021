6a1f681fa494c4e607e4f5d3c7506022afc8a586
dirk <dirk@blubbsoft.de>
2023-12-29 21:22:54 +0100

    day 21 part 2 intermediate step

--- src/aoc2023/Day21.java
+++ src/aoc2023/Day21.java
@@ -10,8 +10,8 @@ public class Day21 {
 
     public static void main(String[] args) throws IOException {
         long startTime = System.currentTimeMillis();
-//        BufferedReader br = new BufferedReader(new FileReader("21-input.txt"));
-        BufferedReader br = new BufferedReader(new FileReader("21-sample.txt"));
+        BufferedReader br = new BufferedReader(new FileReader("21-input.txt"));
+//        BufferedReader br = new BufferedReader(new FileReader("21-sample.txt"));
 
         String s;
         List<String> strings = new ArrayList<>();
@@ -40,7 +40,7 @@ public class Day21 {
         while (steps < 64) {
             Set<Pos> next = new HashSet<>();
             for (Pos p : cur) {
-                p.possibleMovesOne(map, next);
+                p.possibleMoves(map, next, null);
             }
             steps++;
             cur = next;
@@ -50,38 +50,57 @@ public class Day21 {
         long p1Time = System.currentTimeMillis();
         System.out.printf("%5.3f sec\n\n", (p1Time - startTime) / 1000f);
 
+        // 26501365 steps is 202300 * 131 + 65. This is not random - you can reach every side of the starting square in
+        // 65 steps, and 131 further out for another square.
+        // figure out whole squares even/odd
         steps = 0;
         cur.clear();
         cur.add(start);
         HashSet<Pos> even = new HashSet<>();
         HashSet<Pos> odd = new HashSet<>();
-        long time = System.currentTimeMillis();
-        while (steps < 26501365) {
+        while (steps < 131) {
             int rem = steps % 2;
             HashSet<Pos> block = rem == 0 ? even : odd;
             Set<Pos> next = new HashSet<>();
             for (Pos p : cur) {
-                p.possibleMovesTwo(map, next, block);
+                p.possibleMoves(map, next, block);
             }
             block.addAll(next);
             steps++;
             cur = next;
-            if (steps <= 10 || steps == 50 || steps == 100 || steps == 500 || steps % 1000 == 0) {
-                long t = System.currentTimeMillis();
-                System.out.printf("%10d steps: %12d possibilities %8d frontier size %5.3f sec\n", steps, block.size(), cur.size(), (t-time)/1000f);
-                time = t;
-            }
+            System.out.printf("%4d steps: %d even %d odd\n", steps, even.size(), odd.size());
         }
-        /*
-        Wenn man in step s irgendwo ist, kann man immer 2 später wieder da sein. 1 später immer nicht. Das heißt, man
-        muss nicht mehr dorthin gehen!
+        long evenWhole = even.size();
+        long oddWhole = odd.size();
 
-        10708469
-        16733044
-        16733044
-         */
+        // figure out how many even / odd can be reached from the corners
+        even.clear();
+        odd.clear();
+        cur.clear();
+        cur.add(new Pos(0, 0));
+        cur.add(new Pos(0, nCols - 1));
+        cur.add(new Pos(nRows - 1, 0));
+        cur.add(new Pos(nRows - 1, nCols - 1));
+        steps = 0;
+        while (steps < 65) {
+            int rem = steps % 2;
+            HashSet<Pos> block = rem == 0 ? even : odd;
+            Set<Pos> next = new HashSet<>();
+            for (Pos p : cur) {
+                p.possibleMoves(map, next, block);
+            }
+            block.addAll(next);
+            steps++;
+            cur = next;
+            System.out.printf("%4d corner steps: %d even %d odd\n", steps, even.size(), odd.size());
+        }
+        long evenCorners = even.size();
+        long oddCorners = odd.size();
 
-        System.out.println(cur.size());
+        long n = 202300;
+        long result = (n + 1) * (n + 1) * oddWhole + n * n * evenWhole - (n + 1) * oddCorners + n * evenCorners;
+
+        System.out.println(result);
         long endTime = System.currentTimeMillis();
         System.out.printf("%5.3f sec\n\n", (endTime - p1Time) / 1000f);
     }
@@ -103,39 +122,18 @@ public class Day21 {
     }
 
     record Pos(long row, long col) {
-        public void possibleMovesOne(char[][] map, Collection<Pos> out) {
+        public void possibleMoves(char[][] map, Collection<Pos> out, HashSet<Pos> block) {
             int nRows = map.length;
             int nCols = map[0].length;
             for (Direction dir : Direction.values()) {
                 Pos p = dir.walkFrom(this);
+                if (block != null && block.contains(p)) {
+                    continue;
+                }
                 if (p.col < nCols && p.row < nRows && p.col >= 0 && p.row >= 0 && map[(int) p.row][(int) p.col] == '.') {
                     out.add(p);
                 }
             }
         }
-
-        public void possibleMovesTwo(char[][] map, Collection<Pos> out, Collection<Pos> block) {
-            int nRows = map.length;
-            int nCols = map[0].length;
-            for (Direction dir : Direction.values()) {
-                Pos p = dir.walkFrom(this);
-                if (block.contains(p)) {
-                    continue;
-                }
-                int row = (int) (p.row % nRows);
-                if (row < 0) {
-                    row += nRows;
-                }
-                int col = (int) (p.col % nCols);
-                if (col < 0) {
-                    col += nCols;
-                }
-                if (map[row][col] == '.') {
-                    out.add(p);
-                }
-            }
-        }
     }
-
-    enum Part {ONE, TWO}
 }
