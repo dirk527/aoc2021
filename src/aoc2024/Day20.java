f0d77ad31a8d3a2907569062c039871fa4f796a3
dirk <dirk@blubbsoft.de>
2024-12-21 09:28:54 +0100

    Day 20

--- src/aoc2024/Day20.java
+++ src/aoc2024/Day20.java
@@ -9,7 +9,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Day20 {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader("20-ex"));
+        BufferedReader br = new BufferedReader(new FileReader("20-in"));
 
         String s;
         List<List<Character>> map = new ArrayList<>();
@@ -94,73 +94,52 @@ public class Day20 {
         System.out.println(p1);
 
         counts.clear();
-        for (Pos cur = start; !cur.equals(end); ) {
-            Pos next = null;
-            List<Pos> hashes = new ArrayList<>(3);
-            for (Direction direction : Direction.values()) {
-                int rr = cur.row + direction.r;
-                int cc = cur.col + direction.c;
-                Pos candNext = new Pos(rr, cc);
-                if (map.get(rr).get(cc) != '#') {
-                    if (picos.getOrDefault(candNext, Long.MAX_VALUE) == picos.getOrDefault(cur, -10L) + 1) {
-                        next = candNext;
+        for (int rr = 0; rr < map.size(); rr++) {
+            for (int cc = 0; cc < map.get(rr).size(); cc++) {
+                Pos cur = new Pos(rr, cc);
+                if (picos.containsKey(cur)) {
+                    HashSet<Cheat> cheats = findCheats(map, cur, picos);
+                    for (Cheat cheat : cheats) {
+                        long improvement = picos.get(cheat.end) - picos.get(cur) - cheat.dist;
+                        if (improvement > 0) {
+                            counts.computeIfAbsent(improvement, k -> new AtomicInteger(0)).addAndGet(1);
+                        }
                     }
-                } else {
-                    hashes.add(candNext);
                 }
             }
-            HashSet<Cheat> cheats = findCheats(map, cur, hashes, picos);
-            for (Cheat cheat : cheats) {
-                long improvement = picos.get(cheat.end) - picos.get(cur) - cheat.dist;
-                if (improvement > 0) {
-                    counts.computeIfAbsent(improvement, k -> new AtomicInteger(0)).addAndGet(1);
-                }
-                if (improvement == 66) {
-                    System.out.printf("*** 66er %s=%d ->%d-> %s=%d%n", cheat.begin, picos.get(cheat.begin), cheat.dist, cheat.end, picos.get(cheat.end));
-                }
-            }
-            cur = next;
         }
 
-        ArrayList<Long> keys = new ArrayList<>(counts.keySet());
-        keys.sort(Long::compareTo);
-        for (Long key : keys) {
-            System.out.printf("%d way(s) to save %d%n", counts.get(key).get(), key);
+//        ArrayList<Long> keys = new ArrayList<>(counts.keySet());
+//        keys.sort(Long::compareTo);
+//        for (Long key : keys) {
+//            System.out.printf("%d way(s) to save %d%n", counts.get(key).get(), key);
+//        }
+
+        long p2 = 0;
+        for (Long key : counts.keySet()) {
+            if (key >= 100) {
+                p2 += counts.get(key).get();
+            }
         }
+        System.out.println(p2);
+
     }
 
-    private static HashSet<Cheat> findCheats(List<List<Character>> map, Pos start, List<Pos> firstHash, HashMap<Pos, Long> picos) {
+    private static HashSet<Cheat> findCheats(List<List<Character>> map, Pos start, HashMap<Pos, Long> picos) {
         HashSet<Cheat> foundCheats = new HashSet<>();
-        PriorityQueue<State> queue = new PriorityQueue<>();
-        HashSet<Pos> seen = new HashSet<>();
-        HashSet<Pos> seenOnPath = new HashSet<>();
-        firstHash.forEach(pos -> queue.add(new State(pos, 1)));
-        while (!queue.isEmpty()) {
-            State state = queue.poll();
-            if (seen.contains(state.pos)) {
-                continue;
-            }
-            if (state.dist > 19) {
-                continue;
-            }
-            seen.add(state.pos);
-            for (Direction dir : Direction.values()) {
-                int hashr = state.r() + dir.r;
-                int hashc = state.c() + dir.c;
-                if (hashr < 1 || hashr >= map.size() - 1 || hashc < 1 || hashc >= map.get(hashr).size() - 1) {
+        for (int row = start.row - 20; row <= start.row + 20; row++) {
+            for (int col = start.col - 20; col <= start.col + 20; col++) {
+                if (row < 1 || row >= map.size() - 1 || col < 1 || col >= map.get(row).size() - 1) {
                     continue;
                 }
-                Pos move = new Pos(hashr, hashc);
-                if (seen.contains(move)) {
+                int dist = Math.abs(start.row - row) + Math.abs(start.col - col);
+                if (dist > 20) {
                     continue;
                 }
-                if (picos.get(move) != null) {
-                    if (!seenOnPath.contains(move) && picos.get(move) > picos.get(start) + state.dist + 1) {
-                        seenOnPath.add(move);
-                        foundCheats.add(new Cheat(start, move, state.dist + 1));
-                    }
+                Pos cand = new Pos(row, col);
+                if (picos.get(cand) != null && picos.get(cand) > picos.get(start) + dist) {
+                    foundCheats.add(new Cheat(start, cand, dist));
                 }
-                queue.add(new State(move, state.dist + 1));
             }
         }
         return foundCheats;
